704fe4166481c320237191f46094ace1b22ef0ce
ntaklive <ntaklive@mail.ru>
2022-04-28 19:26:23 +0300

    ConsoleCmdBackup.cs refactoring

--- BackupMod/src/Commands/ConsoleCmdBackup.cs
+++ BackupMod/src/Commands/ConsoleCmdBackup.cs
@@ -12,12 +12,12 @@ public class ConsoleCmdBackup : ConsoleCmdAbstract
 {
     private readonly IWorldBackupService _backupService = ServiceLocator.GetRequiredService<IWorldBackupService>();
     private readonly IWorldService _worldService = ServiceLocator.GetRequiredService<IWorldService>();
-    private readonly IChatService _chatService = ServiceLocator.GetRequiredService<IChatService>();
+    private readonly IChatService _chatService = ServiceLocator.GetService<IChatService>();
     private readonly IConfigurationProvider _configurationProvider = ServiceLocator.GetRequiredService<IConfigurationProvider>();
     private readonly ILogger<ConsoleCmdBackup> _logger = ServiceLocator.GetRequiredService<ILogger<ConsoleCmdBackup>>();
 
     public override bool IsExecuteOnClient => false;
-    
+
     public override bool AllowedInMainMenu => true;
 
     public override string[] GetCommands() => new string[2]
@@ -29,33 +29,29 @@ public class ConsoleCmdBackup : ConsoleCmdAbstract
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public override void Execute(List<string> _params, CommandSenderInfo _senderInfo)
     {
-        if (_params.Count == 0)
+        switch (_params.Count)
         {
-            if (_worldService.GetCurrentWorld() != null)
-            {
+            case 0:
                 BackupInternal();
-            }
-            else
-            {
-                _logger.Error("This command can only be executed when a game is started.");
-            }
-            return;
-        }
 
-        if (_params.Count == 1)
-        {
-            if (_params[0] == "info")
+                return;
+            case 1:
             {
-                BackupInfoInternal();
+                if (_params[0] == "info")
+                {
+                    BackupInfoInternal();
+                }
+                else
+                {
+                    _logger.Error("Unknown command. Check that the command is entered correctly.");
+                }
+
+                return;
             }
-            else
-            {
-                _logger.Error("Unknown command. Check that the command is entered correctly.");
-            }
-            return;
+            default:
+                _logger.Error("Unknown command. Wrong amount of arguments.");
+                break;
         }
-        
-        _logger.Error("Unknown command. Wrong amount of arguments.");
     }
 
     public override string GetDescription() => "make a backup of the current save (command from the BackupMod)";
@@ -63,21 +59,28 @@ public class ConsoleCmdBackup : ConsoleCmdAbstract
     private void BackupInfoInternal()
     {
         Configuration configuration = _configurationProvider.GetConfiguration();
-        
+
         _logger.Debug($"AutoBackupDelay: {configuration.AutoBackupDelay.ToString()}");
         _logger.Debug($"BackupsLimit: {configuration.BackupsLimit.ToString()}");
         _logger.Debug($"EnableChatMessages: {configuration.EnableChatMessages.ToString()}");
         _logger.Debug($"BackupOnWorldLoaded: {configuration.BackupOnWorldLoaded.ToString()}");
         _logger.Debug($"CustomBackupsFolder: {configuration.CustomBackupsFolder}");
     }
-    
+
     private void BackupInternal()
     {
-        SaveInfo saveInfo = _worldService.GetCurrentWorldSaveInfo();
-        
-        _backupService.Backup(saveInfo, BackupMode.SaveAllAndBackup);
-        
-        _logger.Debug("The manual backup was successfully completed.");
-        _chatService?.SendMessage("The manual backup was successfully completed.");
+        if (_worldService.GetCurrentWorld() != null)
+        {
+            SaveInfo saveInfo = _worldService.GetCurrentWorldSaveInfo();
+
+            _backupService.Backup(saveInfo, BackupMode.SaveAllAndBackup);
+
+            _logger.Debug("The manual backup was successfully completed.");
+            _chatService?.SendMessage("The manual backup was successfully completed.");
+        }
+        else
+        {
+            _logger.Error("This command can only be executed when a game is started.");
+        }
     }
 }
