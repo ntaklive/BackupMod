a29246ac825c26101a5a617a4aca8b1dbec362cb
ntaklive <ntaklive@mail.ru>
2022-04-28 16:15:43 +0300

    Version bump

--- BackupMod/src/Properties/AssemblyInfo.cs
+++ BackupMod/src/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Backup Mod")]
-[assembly: AssemblyDescription("World auto-backup functionality")]
+[assembly: AssemblyDescription("This modlet makes it possible to automatically backup your game saves")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ntaklive`s Backup Mod")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4")]
-[assembly: AssemblyFileVersion("1.0.4")]
+[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.5")]
