00a07a4d108773ce0ba976d1835949c4aa6e2831
ntaklive <ntaklive@mail.ru>
2022-04-30 01:26:20 +0300

    ArchiveService: 'DecompressZipToFolder' method was added

--- BackupMod/src/Services/Abstractions/Interfaces/IArchiveService.cs
+++ BackupMod/src/Services/Abstractions/Interfaces/IArchiveService.cs
@@ -3,4 +3,5 @@ namespace BackupMod.Services.Abstractions;
 public interface IArchiveService
 {
     public void CompressFolderToZip(string folderPath, string zipPath, bool includeBaseDirectory = true);
+    public void DecompressZipToFolder(string zipPath, string folderPath);
 }
--- BackupMod/src/Services/Implementations/ArchiveService.cs
+++ BackupMod/src/Services/Implementations/ArchiveService.cs
@@ -1,4 +1,5 @@
 using System.IO.Compression;
+using System.Text;
 using BackupMod.Services.Abstractions;
 
 namespace BackupMod.Services;
@@ -7,6 +8,11 @@ public class ArchiveService : IArchiveService
 {
     public void CompressFolderToZip(string folderPath, string zipPath, bool includeBaseDirectory = true)
     {
-        ZipFile.CreateFromDirectory(folderPath, zipPath, CompressionLevel.Optimal, includeBaseDirectory);
+        ZipFile.CreateFromDirectory(folderPath, zipPath, CompressionLevel.Optimal, includeBaseDirectory, Encoding.UTF8);
+    }
+
+    public void DecompressZipToFolder(string zipPath, string folderPath)
+    {
+        ZipFile.ExtractToDirectory(zipPath, folderPath, Encoding.UTF8);
     }
 }
