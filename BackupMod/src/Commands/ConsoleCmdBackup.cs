1e32b7e92783b48080576a936e7a4858e510eb66
ntaklive <ntaklive@mail.ru>
2022-05-01 17:27:24 +0300

    ConsoleCmdBackup: the new '/backup restore' command was added

--- BackupMod/src/Commands/ConsoleCmdBackup.cs
+++ BackupMod/src/Commands/ConsoleCmdBackup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
@@ -6,6 +7,7 @@ using BackupMod.DI;
 using BackupMod.Services.Abstractions;
 using BackupMod.Services.Abstractions.Enum;
 using BackupMod.Services.Abstractions.Models;
+using UniLinq;
 
 namespace BackupMod.Commands;
 
@@ -13,8 +15,13 @@ namespace BackupMod.Commands;
 public class ConsoleCmdBackup : ConsoleCmdAbstract
 {
     private readonly IWorldService _worldService = ServiceLocator.GetRequiredService<IWorldService>();
+    private readonly IDirectoryService _directoryService = ServiceLocator.GetRequiredService<IDirectoryService>();
+
+    private readonly IGameDirectoriesProvider _gameDirectoriesProvider =
+        ServiceLocator.GetRequiredService<IGameDirectoriesProvider>();
+
+    private readonly ISavesProvider _savesProvider = ServiceLocator.GetRequiredService<ISavesProvider>();
     private readonly IChatService _chatService = ServiceLocator.GetService<IChatService>();
-    private readonly IConfigurationProvider _configurationProvider = ServiceLocator.GetRequiredService<IConfigurationProvider>();
     private readonly ILogger<ConsoleCmdBackup> _logger = ServiceLocator.GetRequiredService<ILogger<ConsoleCmdBackup>>();
 
     public override bool IsExecuteOnClient => false;
@@ -42,27 +49,174 @@ public class ConsoleCmdBackup : ConsoleCmdAbstract
                 {
                     BackupInfoInternal();
                 }
+                else if (_params[0] == "restore")
+                {
+                    BackupRestoreInternal(null, null, null);
+                }
                 else
                 {
-                    _logger.Error("Unknown command. Check that the command is entered correctly.");
+                    LogUnknownCommand();
+                }
+
+                return;
+            }
+            case 4:
+            {
+                if (_params[0] == "restore")
+                {
+                    if (!int.TryParse(_params[1], out int worldId))
+                    {
+                        LogInvalidArgument("world id", _params[1]);
+
+                        return;
+                    }
+
+                    if (!int.TryParse(_params[2], out int saveId))
+                    {
+                        LogInvalidArgument("save id", _params[2]);
+
+                        return;
+                    }
+
+                    if (!int.TryParse(_params[3], out int backupId))
+                    {
+                        LogInvalidArgument("backup id", _params[3]);
+
+                        return;
+                    }
+
+                    BackupRestoreInternal(worldId, saveId, backupId);
+                }
+                else
+                {
+                    LogUnknownCommand();
                 }
 
                 return;
             }
             default:
-                _logger.Error("Unknown command. Wrong amount of arguments.");
-                break;
+                LogWrongArgumentsAmount();
+
+                return;
         }
     }
 
-    public override string GetDescription() => "make a backup of the current save (command from the BackupMod)";
+    public override string GetDescription() => "some commands to simplify the creation of backups (command from the BackupMod)";
+
+    private void BackupRestoreInternal(int? worldId, int? saveId, int? backupId)
+    {
+        if (_worldService.GetCurrentWorld() != null)
+        {
+            _logger.Error("This command can only be executed in the main menu.");
+
+            return;
+        }
+
+        WorldInfo[] worlds = _savesProvider.GetAllWorlds().ToArray();
+
+        if (worldId == null || saveId == null || backupId == null)
+        {
+            _logger.Debug("Please specify a backup to restore. Hint: '/backup restore *worldId* *saveId* *backupId*'.");
+
+            PrintAvailableBackups(worlds);
+
+            return;
+        }
+
+        int worldsAmount = worlds.Length;
+        if (worldId >= worldsAmount || worldId < 0)
+        {
+            _logger.Error($"Invalid world id. Please specify a world id in the range 0-{worldsAmount}.");
+            
+            return;
+        }
+
+        int savesAmount = worlds[worldId.Value].Saves.Length;
+        if (saveId >= savesAmount || saveId < 0)
+        {
+            _logger.Error($"Invalid save id. Please specify a save id in the range 0-{savesAmount}.");
+            
+            return;
+        }
+
+        int backupAmount = worlds[worldId.Value].Saves[saveId.Value].Backups.Length;
+        if (backupId >= backupAmount || backupId < 0)
+        {
+            _logger.Error($"Invalid backup id. Please specify a backup id in the range 0-{backupAmount}.");
+            
+            return;
+        }
+
+        var backupService = ServiceLocator.GetRequiredService<IWorldBackupService>();
+
+        try
+        {
+            backupService.RestoreAsync(worlds[worldId.Value].Saves[saveId.Value].Backups[backupId.Value]);
+        }
+        catch (Exception exception)
+        {
+            _logger.Exception(exception);
+            
+            return;
+        }
+        
+        _logger.Debug("The selected save's backup was successfully restored.");
+    }
+    
+    private void PrintAvailableBackups(IReadOnlyList<WorldInfo> worlds)
+    {
+        int worldsAmount = worlds.Count;
+
+        if (worldsAmount != 0)
+        {
+            _logger.Debug("Available backups:");
+        }
+        else
+        {
+            _logger.Debug("There is no worlds.");
+            _logger.Debug("First, you should make a backup of any save.");
+        }
+        
+        for (var i = 0; i < worldsAmount; i++)
+        {
+            WorldInfo world = worlds[i];
+
+            _logger.Debug($"[{i}]: {world.WorldName}");
+
+            int savesAmount = world.Saves.Length;
+            if (savesAmount == 0)
+            {
+                _logger.Debug("    There is no saves.");
+            }
+            
+            for (var j = 0; j < savesAmount; j++)
+            {
+                SaveInfo save = world.Saves[j];
+
+                _logger.Debug($"    [{j}]: {save.SaveName}");
+
+                int backupsAmount = save.Backups.Length;
+                if (backupsAmount == 0)
+                {
+                    _logger.Debug("        There is no backups.");
+                }
+
+                for (var k = 0; k < backupsAmount; k++)
+                {
+                    BackupInfo backup = save.Backups[k];
+
+                    _logger.Debug($"        [{k}]: {backup.Timestamp:yyyy-dd-M--HH-mm-ss}");
+                }
+            }
+        }
+    }
 
     private void BackupInfoInternal()
     {
-        Configuration configuration = _configurationProvider.GetConfiguration();
+        var configuration = ServiceLocator.GetRequiredService<Configuration>();
 
-        var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString(); 
-        
+        var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
         _logger.Debug($"BackupMod v{assemblyVersion.Substring(0, assemblyVersion.Length - 2)} by ntaklive");
         _logger.Debug("Current settings:");
         _logger.Debug($"AutoBackupDelay: {configuration.AutoBackupDelay.ToString()}");
@@ -74,23 +228,29 @@ public class ConsoleCmdBackup : ConsoleCmdAbstract
 
     private async Task BackupInternal()
     {
-       
-        if (_worldService.GetCurrentWorld() != null)
-        {
-            var backupService = ServiceLocator.GetRequiredService<IWorldBackupService>();
-            
-            SaveInfo saveInfo = _worldService.GetCurrentWorldSaveInfo();
-
-            string backupFilePath = await backupService.BackupAsync(saveInfo, BackupMode.SaveAllAndBackup);
-
-            _logger.Debug("The manual backup was successfully completed.");
-            _logger.Debug($"The backup file location: \"{backupFilePath}\".");
-            
-            _chatService?.SendMessage("The manual backup was successfully completed.");
-        }
-        else
+        if (_worldService.GetCurrentWorld() == null)
         {
             _logger.Error("This command can only be executed when a game is started.");
+
+            return;
         }
+
+        var backupService = ServiceLocator.GetRequiredService<IWorldBackupService>();
+
+        SaveInfo saveInfo = _worldService.GetCurrentWorldSaveInfo();
+
+        string backupFilePath = await backupService.BackupAsync(saveInfo, BackupMode.SaveAllAndBackup);
+
+        _logger.Debug("The manual backup was successfully completed.");
+        _logger.Debug($"The backup file location: \"{backupFilePath}\".");
+
+        _chatService?.SendMessage("The manual backup was successfully completed.");
     }
+
+    private void LogInvalidArgument(string argumentName, string argumentValue) =>
+        _logger.Error($"{argumentValue} is an invalid {argumentName}.");
+
+    private void LogUnknownCommand() => _logger.Error("Unknown command. Check that the command is entered correctly.");
+
+    private void LogWrongArgumentsAmount() => _logger.Error("Unknown command. Wrong amount of arguments.");
 }
