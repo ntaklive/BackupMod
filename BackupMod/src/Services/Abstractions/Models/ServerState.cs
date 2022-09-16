008afaf8dd35e80e2e077b7dbc1b0d6c90ee8b6a
ntaklive <foreverzigrik@gmail.com>
2022-09-16 23:36:44 +0300

    Added a constructor to the ServerState model

--- BackupMod/src/Services/Abstractions/Models/ServerState.cs
+++ BackupMod/src/Services/Abstractions/Models/ServerState.cs
@@ -4,6 +4,13 @@ namespace BackupMod.Services.Abstractions.Models;
 
 public struct ServerState
 {
+    public ServerState()
+    {
+        AccessibilityState = ServerAccessibilityState.Unknown;
+        FillingState = ServerFillingState.Unknown;
+        PlayersCount = 0;
+    }
+    
     public ServerAccessibilityState AccessibilityState { get; set; }
 
     public ServerFillingState FillingState { get; set; }
